0f9aac1c2d8f61843ff101283fd0420fef94327e
xzhu0092 <xzhu0092@student.monash.edu>
2025-07-11 16:41:55 +0800

    Update app structure and add Form.vue and style.css

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,12 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-import JSON from './components/JSON.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+//import TheWelcome from './components/TheWelcome.vue'
+//import JSON from './components/JSON.vue'
+import Form from './components/Form.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-    <JSON />
-  </main>
+    <Form />
 </template>
 
 <style scoped>
