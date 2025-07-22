35fc24c9e97817bfdf1a85ffe301cf1647324402
xzhu0092 <xzhu0092@student.monash.edu>
2025-07-22 13:53:08 +0800

    Initial commit with HomeView and router setup

--- assignment2_fit5032_35524243/src/main.js
+++ assignment2_fit5032_35524243/src/main.js
@@ -1,6 +1,12 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router' 
 
-createApp(App).mount('#app')
+import 'materialize-css/dist/css/materialize.min.css'
+import 'materialize-css/dist/js/materialize.min.js'
+
+createApp(App).use(router).mount('#app')
+
+
+
+
---
+++ assignment2_fit5032_35524243/src/views/HomeView.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <h4 class="center-align">Welcome to MindConnect</h4>
+    <div class="row">
+      <div class="col s12 m6 l4">
+        <div class="card hoverable">
+          <div class="card-content">
+            <span class="card-title">For Students</span>
+            <p>Anonymous self-assessment, mental health articles, peer discussions.</p>
+          </div>
+          <div class="card-action">
+            <a href="#" class="blue-text">Learn More</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col s12 m6 l4">
+        <div class="card hoverable">
+          <div class="card-content">
+            <span class="card-title">For Professionals</span>
+            <p>Stress management, mood tracking, professional counseling.</p>
+          </div>
+          <div class="card-action">
+            <a href="#" class="blue-text">Explore</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col s12 m12 l4">
+        <div class="card hoverable">
+          <div class="card-content">
+            <span class="card-title">For Seniors</span>
+            <p>Large text, voice assistance, community support for elderly.</p>
+          </div>
+          <div class="card-action">
+            <a href="#" class="blue-text">Get Started</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script setup>
+// 无需逻辑，仅展示内容
+</script>
