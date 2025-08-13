727915563e656e5e10de3059835c07340162725f
xzhu0092 <xzhu0092@student.monash.edu>
2025-08-13 16:25:02 +0800

    update for config

--- xzhu-library/vite.config.js
+++ xzhu-library/vite.config.js
@@ -6,6 +6,8 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+   base: '/FIT5032_2025_Xuwen_Zhu_35524243/xzhu-library',
+
   plugins: [
     vue(),
     vueDevTools(),
